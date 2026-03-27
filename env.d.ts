64a110ef2b9c53a5430ff0069b5dca44edb4a254
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-03-27 13:51:41 +0100

    refactor: update TypeScript configuration and module declarations

--- env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-declare module '*.svg?component' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
